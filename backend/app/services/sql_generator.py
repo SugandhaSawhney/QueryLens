60fe3950ad4820bb7891bfba9a9347ab38c80b54
Sugandha Sawhney <sawhneysugandha580@gmail.com>
2026-04-28 12:56:50 +0530

    sql_generator modufied

--- backend/app/services/sql_generator.py
+++ backend/app/services/sql_generator.py
@@ -11,7 +11,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
+GROQ_API_URL = os.getenv("GROQ_API_URL")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY not set in .env file")
